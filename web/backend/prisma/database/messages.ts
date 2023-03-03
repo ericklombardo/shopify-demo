ba08331fa7abd328bf76099dcece157985f4d36c
Erick Lombardo <lombardo79@gmail.com>
2023-03-03 07:30:59 -0600

    feat(Backend): add methods for getting, adding, deleting and updating messages

--- web/backend/prisma/database/messages.ts
+++ web/backend/prisma/database/messages.ts
@@ -1,11 +1,50 @@
 import { exec } from "./client";
+import { Message } from "../types";
 
-export const createMessage = (message: string) => {
-  return exec((prisma) =>
-    prisma.message.create({
-      data: {
-        description: message,
-      },
-    })
-  );
+export const messageRepository = {
+  create(message: string): Promise<Message | undefined> {
+    return exec<Message>((prisma) =>
+      prisma.message.create({
+        data: {
+          description: message,
+        },
+      })
+    );
+  },
+  update(id: number, message: string): Promise<Message | undefined> {
+    return exec<Message>((prisma) =>
+      prisma.message.update({
+        where: { id },
+        data: {
+          description: message,
+        },
+      })
+    );
+  },
+  delete(id: number): Promise<Message | undefined> {
+    return exec<Message>((prisma) => prisma.message.delete({ where: { id } }));
+  },
+  getById(id: number): Promise<Message | null | undefined> {
+    return exec<Message | null>((prisma) =>
+      prisma.message.findUnique({ where: { id } })
+    );
+  },
+  getAll(): Promise<Message[] | undefined> {
+    return exec<Message[]>((prisma) =>
+      prisma.message.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+      })
+    );
+  },
+  getLatestMessage(): Promise<Message | null | undefined> {
+    return exec<Message | null>((prisma) =>
+      prisma.message.findFirst({
+        orderBy: {
+          createdAt: "desc",
+        },
+      })
+    );
+  },
 };
