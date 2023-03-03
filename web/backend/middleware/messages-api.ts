dfef4e97a970586b35e303dcafb292aa8572fc63
Erick Lombardo <lombardo79@gmail.com>
2023-03-03 08:08:07 -0600

    feat(Backend): add express middleware for authenticated messages endpoints

---
+++ web/backend/middleware/messages-api.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { MessageRepository } from "../prisma/database";
+
+export const applyMessagesApiEndpoints = (app: express.Express) => {
+  app.use(express.json());
+
+  app.get("/api/messages/:id", async (req, res) => {
+    const { id } = req.params;
+    const message = await MessageRepository.getById(parseInt(id, 10));
+    res.status(200).send(message);
+  });
+
+  app.get("/api/messages", async (req, res) => {
+    const messages = await MessageRepository.getAll();
+    res.status(200).send(messages);
+  });
+
+  app.post("/api/messages", async (req, res) => {
+    const { message } = req.body;
+    const newMessage = await MessageRepository.create(message);
+    res.status(200).send(newMessage);
+  });
+
+  app.patch("/api/messages/:id", async (req, res) => {
+    const { id } = req.params;
+    const { message } = req.body;
+    const updatedMessage = await MessageRepository.update(
+      parseInt(id, 10),
+      message
+    );
+    res.status(200).send(updatedMessage);
+  });
+};
